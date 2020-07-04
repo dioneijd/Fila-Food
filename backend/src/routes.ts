f996decc992727d89dced7ff6abe2521e4e8ccbf
Dionei J. Delangolo <dionei.jd@gmail.com>
2020-07-04 20:00:06 -0300

    Created Session Controller

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 
+
+import SessionController from './controllers/SessionController'
 import RestaurantsController from './controllers/RestaurantsController'
 import CustomersController from './controllers/CustomersController'
 import QueuesController from './controllers/QueuesController'
@@ -7,10 +9,15 @@ import TablesController from './controllers/TablesController'
 
 const routes = express.Router()
 
+const sessionController = new SessionController()
 const restaurantsController = new RestaurantsController()
 const customersController = new CustomersController()
 const tablesController = new TablesController()
 const queuesController = new QueuesController()
+
+
+routes.post('/sessions', sessionController.login)
+
 
 routes.get('/restaurants', restaurantsController.index)
 routes.get('/restaurants/:id', restaurantsController.show)
@@ -18,7 +25,7 @@ routes.post('/restaurants', restaurantsController.store)
 routes.put('/restaurants/:id', restaurantsController.update)
 
 routes.get('/customers', customersController.index)
-routes.get('/customers/:id', customersController.show)
+routes.post('/customers/:email', customersController.show)
 routes.post('/customers', customersController.store)
 routes.put('/customers/:id', customersController.update)
 
