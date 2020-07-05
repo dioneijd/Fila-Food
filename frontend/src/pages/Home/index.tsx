dc04c26a9fd65ddbf927dfb89c5ccc1aae775e9e
Dionei J. Delangolo <dionei.jd@gmail.com>
2020-07-04 21:12:14 -0300

    Login funcionality

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState, FormEvent } from 'react'
 import { FiLogIn } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom'
+import api from '../../services/api'
 
 import logo from '../../assets/logo.svg';
 
-import './styles.css';
+import './styles.css'
+
+interface loginResponse {
+    data: {
+        message: String
+    }
+}
+
 
 const Home = () => {
+
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')  
+    const [showMsg, setShowMsg] =  useState<boolean>(false)
+    const history = useHistory()
+
+    async function handleSubmit(event: FormEvent) {
+        event.preventDefault()
+
+        const restaurant = {
+            type: 'R',
+            email,
+            password
+        }
+
+        try {
+            setShowMsg(false)
+            const login:loginResponse = await api.post('/sessions', restaurant)
+            
+            localStorage.setItem('loginSession', JSON.stringify(login.data))
+            history.push('/control-panel')            
+        } catch {
+            setShowMsg(true)
+        }
+    }
+
+
+
     return (
         <div id="page-home">
             <div className="content">
@@ -14,7 +50,7 @@ const Home = () => {
                     <img src={logo} alt="Fila Food" />
                 </header>
                 <main>
-                    <form /*onSubmit={handleSubmit}*/>
+                    <form onSubmit={handleSubmit}>
                         <h1>Fila digital, sem aglomerações</h1>
                         <br/>
                         <br/>
@@ -24,7 +60,7 @@ const Home = () => {
                                 type="email"
                                 name="email"
                                 id="email"
-                                /*onChange={handleImputChange}*/
+                                onChange={e => setEmail(e.target.value)}
                             />
                         </div>
                         <div className="field">
@@ -33,17 +69,30 @@ const Home = () => {
                                 type="password"
                                 name="password"
                                 id="password"
-                                /*onChange={handleImputChange}*/
+                                onChange={e => setPassword(e.target.value)}
                             />
                         </div>
-                        <div className="btn">
+
+                        {
+                            showMsg && (
+                                <p>
+                                    Login e/ou senha não está correto
+                                </p>
+                            )
+                        }
+                        
+                        <button type="submit">Cadastrar Restaurante</button>
+
+
+
+                        {/* <div className="btn">
                         <Link to="/control-panel">
                             <span>
                                 <FiLogIn />
                             </span>
                             <strong>Entrar</strong>
                         </Link>
-                        </div>
+                        </div> */}
                         <p>Crie uma conta <Link to="/create-restaurant">aqui</Link></p>
                     </form>
                 </main>
